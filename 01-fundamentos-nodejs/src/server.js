c9a8ce7618ac57b637338a3e7fb66c402f707047
MPasti <matheusspasti@gmail.com>
2024-02-23 16:02:35 -0300

    Adicionando logicas

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -10,7 +10,33 @@ import http from "node:http";
 //aqui criamos uma rota, um server http, e recebemos a requisição e a resposta
 //req recebemos informações de quem está chamando
 //res devolvemos uma resposta
+
+const users = [];
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    //early return
+    //não podemos retornar um array, pode ser 3 formatos no maximo, string, buffer ou uint8array
+    //então trabalhamos como JSON, para transitar os dados
+    return res
+      .setHeader("Content-Type", "application/json")
+      .end("Listagem de usuários", JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+
+    return res.end("Criação de de usuários");
+  }
+
+  console.log(method, url);
+
   return res.end("hello world!");
 });
 
